c16abcbc8c023c5b6758ac80721f5cff4a640e29
SheejaBenadict <sheejabenadict16@gmail.com>
2024-08-06 14:40:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+let words = document.querySelectorAll(".word");
+setTimeout(() => {
+    (words[0]).className = "second";
+    words.innerHTML
+    setTimeout(() => {
+        (words[1]).className = "second";
+        setTimeout(() => {
+            (words[2]).className = "second";
+            setTimeout(() => {
+                (words[3]).className = "second";
+                setTimeout(() => {
+                    (words[4]).className = "second";
+                    setTimeout(() => {
+                        (words[5]).className = "second";
+                        setTimeout(() => {
+                            (words[6]).className = "second";
+                            setTimeout(() => {
+                                (words[7]).className = "second";
+                                setTimeout(() => {
+                                    (words[8]).className = "second";
+                                    setTimeout(() => {
+                                        (words[9]).className = "second";
+                                        setTimeout(() => {
+                                            (words[10]).className = "second";
+
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
